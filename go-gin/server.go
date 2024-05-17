ee318356f143932569c5ddfab633f044f0b0aec4
Berm OSG <berm@vps.ovh>
2024-05-17 03:43:23 +0000

    Get PORT env

--- go-gin/server.go
+++ go-gin/server.go
@@ -1,13 +1,23 @@
 package main
 
 import (
+	"os"
+
 	"github.com/gin-gonic/gin"
 )
+
+func getPort() string {
+	value := os.Getenv("PORT")
+	if value == "" {
+		return "8081"
+	}
+	return value
+}
 
 func main() {
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
 		c.String(200, "Hello, Gin!")
 	})
-	r.Run(":8081")
+	r.Run(":" + getPort())
 }
